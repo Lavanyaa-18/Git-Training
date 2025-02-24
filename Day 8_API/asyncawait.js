9c5dfc9e5dfdacbd2942922d9e40e4a82782b1f2
Lavanyaa-18 <lavanyanidhi1803@gmail.com>
2025-02-24 11:32:40 +0530

    Add files via upload

---
+++ Day 8_API/asyncawait.js
@@ -0,0 +1,10 @@
+async function fetchdata(){
+    try{
+        const resp=await fetch('https://jsonplaceholder.typicode.com/users')
+        const json=await resp.json();
+        console.log(json)
+    }catch(error){
+        console.log("err occured",error)
+    }
+}
+fetchdata()
